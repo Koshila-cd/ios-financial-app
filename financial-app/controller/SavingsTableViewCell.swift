1a017b4e0728b9bbc1291a2418d54e43d557108c
Koshila Dissanayake <koshila.2016540@iit.ac.lk>
2020-03-08 19:00:09 +0530

    new

---
+++ financial-app/controller/SavingsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SavingsTableViewCell.swift
+//  financial-app
+//
+//  Created by IIT on 3/7/20.
+//  Copyright © 2020 IIT. All rights reserved.
+//
+
+import UIKit
+
+class SavingsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
